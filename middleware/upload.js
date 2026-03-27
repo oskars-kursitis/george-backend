0ef1bc4de48ec78080cba50d57d79b950de88c28
oskars-kursitis <81645259+oskars-kursitis@users.noreply.github.com>
2026-03-27 21:47:53 +0000

    Remove file type filter

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,15 +3,6 @@ const multer = require('multer');
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: 10 * 1024 * 1024 },
-  fileFilter: (req, file, cb) => {
-    const allowed = /jpeg|jpg|png|webp/;
-    const valid = allowed.test(file.mimetype);
-    if (valid) {
-      cb(null, true);
-    } else {
-      cb(new Error('Only image files are allowed'));
-    }
-  },
 });
 
 module.exports = upload;
